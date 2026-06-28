ca8ee2af7160fbef0116df2da6657410c1f153ae
herdeking <your@email.com>
2026-06-28 14:28:22 +0100

    fix standings season to 2025

--- api/standings.js
+++ api/standings.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
-  const { league } = req.query;
-  const url = `https://api.football-data.org/v4/competitions/${league}/standings`;
+  const { league, season } = req.query;
+  const url = `https://api.football-data.org/v4/competitions/${league}/standings?season=${season || 2025}`;
   try {
     const response = await fetch(url, {
       headers: { 'X-Auth-Token': 'ae94b936902e463b9cd2ca4963dfdb09' }
