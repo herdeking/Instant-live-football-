747cc1b2bd110962d7c8c6fdd143aba7330113bc
herdeking <your@email.com>
2026-06-28 17:03:42 +0100

    switch teams API to api-sports.io

--- api/teams.js
+++ api/teams.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
   const { search } = req.query;
-  const url = `https://api.football-data.org/v4/teams?search=${encodeURIComponent(search)}`;
+  const url = `https://v3.football.api-sports.io/teams?search=${encodeURIComponent(search)}`;
   try {
     const response = await fetch(url, {
-      headers: { 'X-Auth-Token': 'ae94b936902e463b9cd2ca4963dfdb09' }
+      headers: { 'x-apisports-key': '14fc22d6286abe7f65bd37725b8fb926' }
     });
     const data = await response.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
